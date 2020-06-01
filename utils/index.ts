fcc62264e88de2d8cef225cfe4f84a906ca63bc3
Griko Nibras <git@griko.id>
2020-06-01 20:54:34 +0700

    Convert to dynamic proxy

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,32 @@
 import { HandleError } from "../types";
 
 export const BASE_URLS = {
-  ["production"]: "https://web.denoland.id",
-  ["prod"]: "https://web.denoland.id",
-
-  ["development"]: "https://web-staging.denoland.id",
-  ["dev"]: "https://web-staging.denoland.id",
+  production: "https://web.denoland.id",
+  prod: "https://web.denoland.id",
+  development: "https://web-staging.denoland.id",
+  dev: "https://web-staging.denoland.id",
 };
+
+export const FWD_REQUEST_HEADERS = [
+  "accept-encoding",
+  "accept-language",
+  "accept",
+  "cache-control",
+  "referer",
+  "upgrade-insecure-requests",
+  "user-agent",
+];
+
+export const FWD_RESPONSE_HEADERS = [
+  "accept-ranges",
+  "access-control-allow-origin",
+  "age",
+  "cache-control",
+  "content-disposition",
+  "content-type",
+  "etag",
+  "strict-transport-security",
+];
 
 export const getBaseUrl = () => {
   return process.env.PROXY_BASE_URL || BASE_URLS[process.env.NODE_ENV];
