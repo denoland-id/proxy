6f3c08dc24f310245419a4655d7ef029a5bc0869
Griko Nibras <git@griko.id>
2020-06-01 19:38:54 +0700

    Add base urls as env vars

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,17 @@
 import { HandleError } from "../types";
 
+export const BASE_URLS = {
+  ["production"]: "https://web.denoland.id",
+  ["prod"]: "https://web.denoland.id",
+
+  ["development"]: "https://web-staging.denoland.id",
+  ["dev"]: "https://web-staging.denoland.id",
+};
+
+export const getBaseUrl = () => {
+  return process.env.PROXY_BASE_URL || BASE_URLS[process.env.NODE_ENV];
+};
+
 export const withHandleError: HandleError = (handler) => (req, res) => {
   try {
     return handler(req, res);
